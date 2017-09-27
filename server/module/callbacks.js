4b47083822afcb25c8665b39109a3ee9971cb457
Alec Sands <sandsalecv@gmail.com>
2017-09-27 13:41:38 -0500

    setup a basic callback function

--- server/module/callbacks.js
+++ server/module/callbacks.js
@@ -1,6 +1,22 @@
+function basicCallback (string, callback) {
+  var type = '';
+  if (typeof string == 'string') {
+    type = true;
+  } else {
+    type = false;
+  }
+  callback(type);
+}
+
+function returnOutcome (response) {
+  if (response == true) {
+    return "correct";
+  } else if (response == false) {
+    return "not correct";
+  } else {
+    return TypeError('expected a boolean');
+  }
+}
 
 
-
-
-
-module.exports = ;
+module.exports = basicCallback;
---
+++ test/callback.test.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var should = chai.should();
+var callbackFunction = require('../server/module/callback.js');
+
+describe('Testing a Callback Function', function() {
+  context('callback.js', function() {
+
+  });
+});
