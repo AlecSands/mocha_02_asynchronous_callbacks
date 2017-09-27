1f7627e0f4e6b3578acfd85e96262c21afce0b53
Alec Sands <sandsalecv@gmail.com>
2017-09-27 14:14:53 -0500

    got a basic test working

--- server/module/callbacks.js
+++ server/module/callbacks.js
@@ -1,34 +1,39 @@
 allFunctions = {};
 
-function basicCallback (string, callback) {
-  var type = '';
-  if (typeof string == 'string') {
-    type = true;
-  } else {
-    type = false;
-  }
-  callback(type);
-}
+// function basicCallback (string, callback) {
+//   var type = '';
+//   if (typeof string == 'string') {
+//     type = true;
+//   } else {
+//     type = false;
+//   }
+//   callback(type);
+// }
+//
+// function returnOutcome (response) {
+//   if (response == true) {
+//     return "correct";
+//   } else if (response == false) {
+//     return "not correct";
+//   } else {
+//     return TypeError('expected a boolean');
+//   }
+// }
+//
+// allFunctions.greeting = function (name) {
+//   alert('Hello ' + name);
+// };
+//
+// allFunctions.processUserInput = function(callback) {
+//   var name = prompt('Please enter your name.');
+//   callback(name);
+// };
+//
+// allFunctions.processUserInput(greeting);
 
-function returnOutcome (response) {
-  if (response == true) {
-    return "correct";
-  } else if (response == false) {
-    return "not correct";
-  } else {
-    return TypeError('expected a boolean');
-  }
-}
-
-allFunctions.greeting = function (name) {
-  alert('Hello ' + name);
+allFunctions.simpleFunction = function(input) {
+  var typeOfInput = typeof input;
+  return typeOfInput;
 };
 
-allFunctions.processUserInput = function(callback) {
-  var name = prompt('Please enter your name.');
-  callback(name);
-};
-
-allFunctions.processUserInput(greeting);
-
-module.exports = {testFunction: basicCallback, testCallback: returnOutcome};
+module.exports = allFunctions;
--- test/callback.test.js
+++ test/callback.test.js
@@ -1,16 +1,26 @@
 var chai = require('chai');
+var expect = require('chai').expect;
 var should = chai.should();
 var callbackFunction = require('../server/module/callbacks.js');
+var simpleFunction = callbackFunction.simpleFunction;
 
-describe('Testing a Callback Function', function() {
-  context('callback.js', function() {
-    it('should return "correct" when passed a string', function(done) {
-      var testString = callbackFunction.testFunction('some string', callbackFunction.testCallback);
-
-      testString.validator(function(error, value) {
-        value.should.deep.equal(testString);
-        done(error);
-      });
+describe('Basic Tests', function() {
+  context('The simpleFunction function', function () {
+    it('should return "string" when passed a string.', function () {
+      expect(simpleFunction('some string')).to.equal('string');
     });
   });
 });
+
+// describe('Testing a Callback Function', function() {
+//   context('callback.js', function() {
+//     it('should return "correct" when passed a string', function(done) {
+//       var testString = callbackFunction.testFunction('some string', callbackFunction.testCallback);
+//
+//       testString.validator(function(error, value) {
+//         value.should.deep.equal(testString);
+//         done(error);
+//       });
+//     });
+//   });
+// });
